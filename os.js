ac21c3cc51e3a0005596ba3cbeda19b14a930a94
Piyush <piyushsuthar6300@gmail.com>
2025-02-10 20:48:54 +0530

    os module

---
+++ os.js
@@ -0,0 +1,11 @@
+const os = require('os')
+
+console.log(os.type())
+console.log(os.platform())
+console.log(os.arch())
+console.log(os.totalmem())
+console.log(os.freemem())
+console.log(os.homedir())
+console.log(os.uptime())
+console.log(os.cpus().length)
+
